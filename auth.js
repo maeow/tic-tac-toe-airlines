6e6ebcaaf68f207d8f4571e3cc928f2b008ee35b
S.sairoong <73654933+Sairoong8520@users.noreply.github.com>
2023-04-04 12:58:11 +0700

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,84 @@
+firebase.auth().onAuthStateChanged((user) => {
+    if(user){
+        console.log("User : ", user);
+        getList(user);
+        console.log('State: yes')
+    }
+    setupUI(user);
+});
+
+const signupForm = document.querySelector("#signup-form");
+signupForm.addEventListener("submit", createUser);
+const signupFeedback = document.querySelector("#feedback-msg-signup");
+const signupModal = new bootstrap.Modal(document.querySelector("#modal-signup"));
+
+function createUser(event){
+    event.preventDefault();
+    const email = signupForm["input-email-signup"].value;
+    const password = signupForm["input-password-signup"].value;
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(() => {
+        console.log("signup")
+        signupFeedback.style = "color: green";
+        signupFeedback.innerHTML = "<i class='bi bi-check-circle-fill'></i> signup completed.";
+        signupForm.reset();
+        setTimeout(() => {
+            signupModal.hide();
+        }, 1000)
+    })
+    .catch((error) => {
+        console.log('invalid')
+        signupFeedback.style = "color: red";
+        signupFeedback.innerHTML = `<i class='bi bi-exclamation-triangle-fill'></i> ${error.message}`
+        signupForm.reset();
+    })
+}
+
+//Cancel
+const btnCancels = document.querySelectorAll(".btn-cancel");
+btnCancels.forEach((btn) => {
+    btn.addEventListener("click",() => {
+        signupForm.reset();
+        signupFeedback.innerHTML = "";
+        loginForm.reset();
+        loginFeedback.innerHTML = "";
+        console.log("Cancel")
+    })
+});
+
+//Logout
+const btnLogout = document.querySelector("#btnLogout");
+btnLogout.addEventListener("click", function(){
+    firebase.auth().signOut();
+    console.log("Logout complete.");
+})
+
+const loginForm = document.querySelector("#login-form");
+loginForm.addEventListener("submit", loginUser);
+
+const loginFeedback = document.querySelector("#feedback-msg-login");
+const loginModal = new bootstrap.Modal(document.querySelector("#modal-login"));
+
+function loginUser(event){
+    event.preventDefault();
+    const email = loginForm["input-email-login"].value;
+    const password = loginForm["input-password-login"].value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(() => {
+        console.log("login")
+        loginFeedback.style = "color: green";
+        loginFeedback.innerHTML = "<i class='bi bi-check-circle-fill'></i> login succeed!.";
+        loginForm.reset();
+        setTimeout(() => {
+            loginModal.hide();
+        }, 1000)
+    })
+    .catch((error) => {
+        console.log('invalid')
+        loginFeedback.style = "color: red";
+        loginFeedback.innerHTML = `<i class='bi bi-exclamation-triangle-fill'></i> ${error.message}`
+        loginForm.reset();
+    })
+}
